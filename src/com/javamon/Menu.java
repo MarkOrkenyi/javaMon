072e2a1f00dd8d3d5c68d287524af75cd08a7a3e
Gina <paalginaa@gmail.com>
2017-08-15 13:58:56 +0200

    menu asks for number until get correct data

--- src/com/javamon/Menu.java
+++ src/com/javamon/Menu.java
@@ -5,34 +5,44 @@ import java.util.Scanner;
 public class Menu {
 
     public static void printMenu () {
-        String menuItem = "1.) Start game\n" +
-                "2.) High scores\n" +
-                "3.) Registration\n" +
-                "4.) User login";
-        System.out.println(menuItem);
 
-        Scanner userInput = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        boolean incorrectData = true;
 
-        if (userInput.hasNextInt()) {
-            int enteredNum = userInput.nextInt();
-            if (enteredNum == 1) {
-                //starts the game
-                System.out.println("start");
-            } else if (enteredNum == 2) {
-                // shows high score
-                System.out.println("highscore");
-            } else if (enteredNum == 3) {
-                // user registration
-                System.out.println("reg");
-            } else if (enteredNum == 4){
-                // user login
-                System.out.print("login");
+        while (incorrectData == true ) {
+            String menuItem = "1.) Start game\n" +
+                    "2.) High scores\n" +
+                    "3.) Registration\n" +
+                    "4.) User login";
+            System.out.println(menuItem);
+
+            Scanner userInput = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+
+            if (userInput.hasNextInt()) {
+                int enteredNum = userInput.nextInt();
+                if (enteredNum == 1) {
+                    //starts game
+                    incorrectData = false;
+                    System.out.println("start");
+
+                } else if (enteredNum == 2) {
+                    // shows high score
+                    incorrectData = false;
+                    System.out.println("highscore");
+                } else if (enteredNum == 3) {
+                    // user registration
+                    incorrectData = false;
+                    System.out.println("reg");
+                } else if (enteredNum == 4) {
+                    // user login
+                    incorrectData = false;
+                    System.out.print("login");
+                } else {
+                    System.out.println("choose between 1, 2 or 3");
+                }
             } else {
-                System.out.println("choose between 1, 2 or 3");
+                System.out.println("not number, try again");
             }
-        } else {
-            System.out.println("not number");
         }
     }
 }
