3bbef844e485680bc80d89f00a516aae6d8506cd
Gina <paalginaa@gmail.com>
2017-08-15 13:44:44 +0200

    user login menupoint added

--- src/com/javamon/Menu.java
+++ src/com/javamon/Menu.java
@@ -7,7 +7,8 @@ public class Menu {
     public static void printMenu () {
         String menuItem = "1.) Start game\n" +
                 "2.) High scores\n" +
-                "3.) Registration";
+                "3.) Registration\n" +
+                "4.) User login";
         System.out.println(menuItem);
 
         Scanner userInput = new Scanner(System.in);
@@ -15,6 +16,21 @@ public class Menu {
 
         if (userInput.hasNextInt()) {
             int enteredNum = userInput.nextInt();
+            if (enteredNum == 1) {
+                //starts the game
+                System.out.println("start");
+            } else if (enteredNum == 2) {
+                // shows high score
+                System.out.println("highscore");
+            } else if (enteredNum == 3) {
+                // user registration
+                System.out.println("reg");
+            } else if (enteredNum == 4){
+                // user login
+                System.out.print("login");
+            } else {
+                System.out.println("choose between 1, 2 or 3");
+            }
         } else {
             System.out.println("not number");
         }
