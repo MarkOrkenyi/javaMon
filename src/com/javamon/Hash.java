344fd9c2472d63cffe717b1091fc1e5240877189
kelliartamas <kelliar.tamas@gmail.com>
2017-08-16 10:39:54 +0200

    Refactoring in HashPass and CheckHash

--- src/com/javamon/Hash.java
+++ src/com/javamon/Hash.java
@@ -5,15 +5,15 @@ import static com.javamon.BCrypt.*;
 public class Hash {
 
     public String HashPass (String password) {
-        String asd = hashpw(password, gensalt(10));
-        return asd;
+        String hashedPw = hashpw(password, gensalt(10));
+        return hashedPw;
     }
 
     public void CheckHash (String candidate, String hashed) {
         if (checkpw(candidate, hashed)) {
-            System.out.println("It matches");
+            System.out.println("Logged in successfully!");
         } else
-            System.out.println("It does not match LOL");
+            System.out.println("Wrong username or password!");
     }
 
 }
