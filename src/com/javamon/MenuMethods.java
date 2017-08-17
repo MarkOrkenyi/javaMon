89e4a50a85ad7d87b140954ed5f246457c1cf16e
Gina <paalginaa@gmail.com>
2017-08-17 20:46:57 +0200

    remove unnecessary prints

--- src/com/javamon/MenuMethods.java
+++ src/com/javamon/MenuMethods.java
@@ -22,10 +22,8 @@ public class MenuMethods {
                 ArrayList randomPhraseData = Queries.getRandomPhrase();
                 String randomPhrase = String.format("What is %s", randomPhraseData.get(0));
                 int correctAnswer;
-                System.out.println(randomPhraseData.get(1));
                 if (randomPhraseData.get(1).equals("pokemon")) {
                     correctAnswer = 1;
-                    System.out.println(correctAnswer);
                 } else {
                     correctAnswer = 2;
                 }
@@ -37,11 +35,9 @@ public class MenuMethods {
                 if(userAnswer == 1 || userAnswer == 2) {
                     if (userAnswer == correctAnswer) {
                         earnedPoints++;
-                        System.out.println(correctAnswer);
                         System.out.println("CORRECT! \n");
                     } else {
                         earnedPoints--;
-                        System.out.println(correctAnswer);
                         System.out.println("INCORRECT!\n");
                     }
                 }else {
